a6520b06f78492f8ac0c04273efb1cfa27803fe9
raupersan <raulps1798@gmail.com>
2024-11-20 12:55:14 +0100

    funciona barra de carga

---
+++ Proyecto_Mecanografia_Raul_Perez/src/ventana/BarraCarga.java
@@ -0,0 +1,19 @@
+package ventana;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+
+public class BarraCarga extends JPanel {
+	
+}
--- Proyecto_Mecanografia_Raul_Perez/src/ventana/Frame.java
+++ Proyecto_Mecanografia_Raul_Perez/src/ventana/Frame.java
@@ -3,7 +3,11 @@ package ventana;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 import javax.swing.Timer;
@@ -15,43 +19,41 @@ import javax.swing.JPanel;
 import java.awt.FlowLayout;
 
 public class Frame {
-	Timer timer;
-
 	public JFrame frame;
+	public Timer timer;
 
 	public Frame() {
 		initialize();
 	}
+
 	public void initialize() {
 		frame = new JFrame();
 		frame.setResizable(false);
 		frame.setBounds(0, 0, 2000, 1080);
 
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
 		JPanel panel = new JPanel();
 		panel.setBounds(829, 939, 314, 44);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
-		JProgressBar barraCarga = new JProgressBar();
-		barraCarga.setBounds(0, 0, 314, 44);
-		barraCarga.setValue(0);
-		barraCarga.setStringPainted(true);
-		barraCarga.setMaximum(6);
-		panel.add(barraCarga);
+
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		progressBar.setBounds(0, 0, 314, 44);
+		panel.add(progressBar);
+		progressBar.setValue(0);
+		progressBar.setMaximum(6);
 		timer = new Timer(1000, new ActionListener() {
-			int i = barraCarga.getMaximum();
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				i--;
-				barraCarga.setValue(barraCarga.getValue() + 1);
-				if (i == 0) {
+				progressBar.setValue(progressBar.getValue() + 1);
+				if (progressBar.getValue() == progressBar.getMaximum()) {
+					BufferedImage imagen = null;
 					timer.stop();
 				}
-				barraCarga.addChangeListener(new ChangeListener() {
+				progressBar.addChangeListener(new ChangeListener() {
 					public void stateChanged(ChangeEvent e) {
 					}
 				});
